2d357db6d458a633aa6083e709d02ac05541f4e6
Mouaad020 <mouaad.chakroun@hva.nl>
2025-12-05 10:40:02 +0100

    popup gefixt

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 //Selecteer de class
 const share = document.querySelector('.sharebutton');
+const sharepopup = document.querySelector('.popup');
 
 //Voeg een click functie toe
 share.addEventListener('click', function() {
     //Controleer of het werkt
     console.log('it works')
-});
+
+   sharepopup.classList.toggle('yes'); 
+})
+
