bdc1c6bf7cecda22c1beaef7b53385a78f5333a0
Mouaad020 <mouaad.chakroun@hva.nl>
2025-12-05 09:52:15 +0100

    Javascript click

---
+++ index.js
@@ -0,0 +1,8 @@
+//Selecteer de class
+const share = document.querySelector('.sharebutton');
+
+//Voeg een click functie toe
+share.addEventListener('click', function() {
+    //Controleer of het werkt
+    console.log('it works')
+});
