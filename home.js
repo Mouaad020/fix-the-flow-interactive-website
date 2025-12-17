3f59e6f8173323a7431e6ab04e8d93dc7f1565af
Mouaad020 <mouaad.chakroun@hva.nl>
2025-12-17 12:18:33 +0100

    #21 chatbot js af

--- home.js
+++ home.js
@@ -1,12 +1,27 @@
 const search = document.querySelector('.button-chat');
 const chatbot = document.querySelector('.aichatbot');
 
-search.addEventListener('click', test) 
-    function test (event){
-    console.log('it works');
+search.addEventListener('click', test)
+function test(event) {
     event.preventDefault();
 
-chatbot.classList.toggle('open');
-    
+    chatbot.classList.toggle('open');
+
 };
 
+const none = document.querySelector('.button-chat');
+
+none.addEventListener('click', function () {
+
+    if (document.querySelector('main header').classList.value == "") {
+
+        document.querySelector('main header').classList.add('disappear');
+    }
+    else {
+        document.querySelector('main header').classList.remove('disappear');
+    }
+
+
+
+
+});
